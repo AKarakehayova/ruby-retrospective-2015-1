af707362e3b40de141dbafb5ff212cc74f1b94a6
AKarakehayova <alexzk95@gmail.com>
2016-01-27 23:06:56 +0200

    upgraded solution

--- solutions/02.rb
+++ solutions/02.rb
@@ -1,46 +1,35 @@
 def move(snake, direction)
-  new_snake = Array.new(snake)
-  new_snake.delete_at(0)
-  length = new_snake.length
-  new_snake[length] = next_position(new_snake, direction)
-  new_snake
+  grow(snake, direction).drop(1)
 end
 
 def next_position(snake, direction)
   [snake.last, direction].transpose.map { |x| x.reduce :+ }
 end
 
-
 def grow(snake, direction)
-  new_snake = Array.new(snake)
-  length = new_snake.length
-  new_snake[length] = next_position(new_snake, direction)
-  new_snake
+  snake.dup.push(next_position(snake, direction))
 end
 
 def new_food(food, snake, dimensions)
-  generated_food = [rand(dimensions[:width]), rand(dimensions[:height])]
-  part_of_the_snake = snake.include? generated_food
-  already_exists = (food - generated_food).empty?
-  if !part_of_the_snake and !already_exists
-    generated_food
-  else new_food(food, snake, dimensions)
-  end
+  xs, ys = (0...dimensions[:width]).to_a, (0...dimensions[:height]).to_a
+  valid_positions = xs.product(ys)
+  empty_positions = valid_positions - (food + snake)
+  empty_positions.sample
 end
 
 def obstacle_ahead?(snake, direction, dimensions)
-  move = next_position(snake, direction)
-  if snake.include? move or move[0] < 0 or move[1] < 0
-    true
-  elsif move[0] >= dimensions[:width] or move[1] >= dimensions[:height]
-    true
-  else false
-  end
+  movement = next_position(snake, direction)
+  wall_ahead?(movement, dimensions) or snake.include?(movement)
 end
 
-def danger? (snake, direction, dimensions)
+def wall_ahead?(position, dimensions)
+  x, y = position
+  x < 0 or x >= dimensions[:width] or y < 0 or y >= dimensions[:height]
+end
+
+def danger?(snake, direction, dimensions)
   dead_after_one_move = obstacle_ahead?(snake, direction, dimensions)
-  new_snake = move(snake,direction)
+  new_snake = move(snake, direction)
   dead_after_two_moves = obstacle_ahead?(new_snake, direction, dimensions)
   dead_after_one_move or dead_after_two_moves
 end
