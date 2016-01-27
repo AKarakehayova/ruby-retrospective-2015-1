c2f9a68bdfde941637de77738ab8449589889be6
AKarakehayova <alexzk95@gmail.com>
2016-01-27 23:05:32 +0200

    upgraded solution

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,11 +1,7 @@
+EXCHANGE_RATE = {bgn: 1, usd: 1.7408, eur: 1.9557, gbp: 2.6415}
+
 def convert_to_bgn(price, currency)
-  result = case currency
-            when :usd then price * 1.7408
-            when :eur then price * 1.9557
-            when :gbp then price * 2.6415
-            when :bgn then price
-           end
-  result.round(2)
+  (price * EXCHANGE_RATE[currency]).round(2)
 end
 
 def compare_prices(price, currency, price_two, currency_two)
